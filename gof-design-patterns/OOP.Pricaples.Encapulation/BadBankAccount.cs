43d2e5149b12617a010b67c8954e173a4f2d1d06
chromaticabberation <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-08-19 02:42:49 +0300

    Add NuGet support files for project restoration
    - Created `gof-design-patterns.csproj.nuget.g.props` to define properties for NuGet package restoration.
    - Added `gof-design-patterns.csproj.nuget.g.targets` for build targets related to NuGet.
    - Introduced `project.assets.json` to manage project dependencies and assets.
    - Generated `project.nuget.cache` to cache NuGet restore results and improve performance.

---
+++ gof-design-patterns/OOP.Pricaples.Encapulation/BadBankAccount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gof_design_patterns.OOP.Pricaples.Encapulation
+{
+    internal class BadBankAccount
+    {
+        public decimal balance = 0;
+    }
+}
---
+++ gof-design-patterns/Program.cs
@@ -0,0 +1,7 @@
+﻿using gof_design_patterns.OOP.Pricaples.Encapulation;
+
+// Encapsulation is a fundamental principle of object-oriented programming that restricts direct access to an object's internal state and behavior. It allows you to control how the internal state of an object is accessed and modified, promoting data integrity and security.
+BadBankAccount alex = new BadBankAccount();
+Console.WriteLine(alex.balance);
+alex.balance = 100000;
+Console.WriteLine(alex.balance);
