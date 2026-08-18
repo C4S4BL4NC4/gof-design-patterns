0d2eeab4fe3084449c10a00d07c562a248ad1258
chromaticabberation <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-08-19 02:54:58 +0300

    Refactor project structure and update source files
    - Removed the BadBankAccount.cs file from the OOP.Pricaples.Encapulation directory.
    - Updated DocumentLayout.json and DocumentLayout.backup.json to reflect new file paths.
    - Added new .vsidx files for updated project structure.
    - Updated binary files in the .vs and obj directories.
    - Added sourcelink.json for source linking to GitHub.
    - Updated AssemblyInfo.cs with new informational version.

--- gof-design-patterns/obj/Debug/net10.0/gof-design-patterns.AssemblyInfo.cs
+++ gof-design-patterns/obj/Debug/net10.0/gof-design-patterns.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gof-design-patterns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43d2e5149b12617a010b67c8954e173a4f2d1d06")]
 [assembly: System.Reflection.AssemblyProductAttribute("gof-design-patterns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gof-design-patterns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
