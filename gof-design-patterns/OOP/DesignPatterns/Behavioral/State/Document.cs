80d6811c2787a0f3a4e47ff52f5a54b8b995689c
chromaticabberation <74897764+C4S4BL4NC4@users.noreply.github.com>
2026-08-22 00:50:32 +0300

    State Pattern
    State Pattern - added comment

--- gof-design-patterns/OOP/DesignPatterns/Behavioral/State/Document.cs
+++ gof-design-patterns/OOP/DesignPatterns/Behavioral/State/Document.cs
@@ -6,6 +6,7 @@ namespace gof_design_patterns.OOP.DesignPatterns.Behavioral.State
 {
     internal class Document
     {
+        // The main idea is that we don't to modify the Document class when we add new states. We just need to create a new state class that implements the State interface and change the state of the document to the new state.
         public State State { get; set; }
         public UserRoles UserRole { get; set; }
 
